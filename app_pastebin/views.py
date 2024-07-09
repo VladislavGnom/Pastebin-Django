e9336f26f5f71fb20a4b8b2a898a567afd2100f1
Vladislav <vladfatov1054@gmail.com>
2024-07-09 13:44:28 +0300

    Upgrade for basic structure

--- app_pastebin/views.py
+++ app_pastebin/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Paste
 
-# Create your views here.
+
+def index(request):
+    pastes = Paste.objects.all()
+
+    context = {
+        'pastes': pastes,
+    }
+
+    return render(request, 'app_pastebin/index.html', context=context)
+
+
+def new_paste(request):
+
+    context = {}
+
+    return render(request, 'app_pastebin/new_paste.html', context=context)
